b94fa3f984c86740dc0ddc3bb061d8a0cd296b79
PhineasMun <123189426+PhineasMun@users.noreply.github.com>
2025-11-24 01:51:48 +0000

    Added all states of GameState

---
+++ src/main/java/com/project/AcceptAnswer.java
@@ -0,0 +1,10 @@
+package com.project;
+
+public class AcceptAnswer implements GameState {
+    @Override
+    public void handleRequest() {
+        // Implementation for accepting an answer
+        System.out.println("Answer accepted.");
+    }
+    
+}
---
+++ src/main/java/com/project/EvaluateAnswer.java
@@ -0,0 +1,10 @@
+package com.project;
+
+public class EvaluateAnswer implements GameState {
+    @Override
+    public void handleRequest() {
+        // Implementation for evaluating an answer
+        System.out.println("Answer evaluated.");
+    }
+    
+}
